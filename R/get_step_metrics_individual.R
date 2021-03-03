ef9735faca47b0f7cd3d5071d9640ef984eb2250
Alexander Vining <aqvining@ucdavis.edu>
2021-03-03 14:37:51 +0100

    initial function documentation

--- R/get_step_metrics_individual.R
+++ R/get_step_metrics_individual.R
@@ -1,10 +1,16 @@
+#' Movement Metric Calculator Individual
+#'
+#' calculates distance traveled between all consecutive points (rows) in the data frame
+#'
+#' @param individual_movement_data data_frame with at least two columns containing x and y coordinate data.
+#' @param x character
+#' @param y character
+#' @param step_number character
+#'
+#' @return data frame
+#' @export
+#'
 get_step_metrics_individual <- function(individual_movement_data, x = "X", y = "Y", step_number = NA) {
-  #input: individual movement data; a data_frame with at least two columns containing x and y coordinate data.
-  #       x; character giving name of column with x coordinates
-  #       y; character giving name of column with y coordinates
-  #       step_number: name of a column with the ordered number of steps. Used to ensure proper sorting
-  #output: a data frame of same structure as individual_movement_data with an additional columns named "Step_length" and "Step_Turn", the latter in radians
-  #description: calculates distance traveled between all consecutive points (rows) in the data frame
   if (! is.na(step_number)) individual_movement_data <- individual_movement_data[order(individual_movement_data[,step_number]),] #sort by step number
   diff_x <- diff(individual_movement_data[[x]])
   diff_y <- diff(individual_movement_data[[y]])
--- R/get_step_metrics_multiple.R
+++ R/get_step_metrics_multiple.R
@@ -1,12 +1,24 @@
+#' Movement Metric Calculator
+#'
+#' calculates distance traveled between all consecutive points (rows) in the data frame, with breaks by individual identifiers
+#'
+#' @param movement_data a data_frame with at least 3 columns containing individual identifiers, x, and y coordinate data.
+#' @param id character giving name of column with unique identifiers
+#' @param x character giving name of column with x coordinates
+#' @param y haracter giving name of column with y coordinates
+#' @param step_number name of a column with the ordered number of steps. Used to ensure proper sorting
+#'
+#' @return data frame
+#' @export
+#'
+#' @examples
+#' move_data1 <- data.frame(x = rnorm(10), y = rnorm(10), Individual = "Subject1", step = 1:10)
+#' move_data2 <- data.frame(x = rnorm(10), y = rnorm(10), Individual = "Subject2", step = 1:10)
+#' get_step_metrics_multiple(rbind(move_data1, move_data2), ID = "Individual", x = "x", y = "y", step_number = "step"
 get_step_metrics_multiple <- function(movement_data, id = "ID", x = "X", y = "Y", step_number = NA) {
-  #input: movement_data; a data_frame with at least 3 columns containing individual identifiers, x, and y coordinate data.
-  #       id; character givine name of column with unique identifyers
-  #       x; character giving name of column with x coordinates
-  #       y; character giving name of column with y coordinates
-  #       step_number: name of a column with the ordered number of steps. Used to ensure proper sorting
-  #output: a data frame of same structure as individual_movement_data with an additional columns named "Step_length" and "Step_Turn"
-  #description: calculates distance traveled between all consecutive points (rows) in the data frame, with breaks by individual identifiers
-  movement_data <- movement_data %>% group_by_at(id) %>% nest() %>% #break dataframe into groups by ID and store into nested data frame for function mapping
+  movement_data <- movement_data %>%
+    group_by_at(id) %>%
+    nest() %>% #break dataframe into groups by ID and store into nested data frame for function mapping
     mutate(data = map(data, get_step_metrics_individual, x, y , step_number)) %>% #apply get_step_metrics_individual to each each data frame stored in data column of nested data frame
     unnest(cols = c(data))
   return(movement_data)
