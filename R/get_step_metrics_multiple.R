55625c7f16c8dab4f9672eb8b4d570e39f606f37
Alexander Vining <aqvining@ucdavis.edu>
2021-06-08 16:14:17 +0200

    added modify_by_group

--- R/get_step_metrics_multiple.R
+++ R/get_step_metrics_multiple.R
@@ -1,20 +1,23 @@
-#' Movement Metric Calculator
+#' @title Movement Metric Calculator
 #'
-#' calculates distance traveled between all consecutive points (rows) in the data frame, with breaks by individual identifiers
+#' @description calculates distance traveled between all consecutive points (rows) in the data frame, with breaks by individual identifiers
 #'
 #' @param movement_data a data_frame with at least 3 columns containing individual identifiers, x, and y coordinate data.
 #' @param id character giving name of column with unique identifiers
 #' @param x character giving name of column with x coordinates
-#' @param y haracter giving name of column with y coordinates
+#' @param y character giving name of column with y coordinates
 #' @param step_number name of a column with the ordered number of steps. Used to ensure proper sorting
 #'
 #' @return data frame
 #' @export
+#' @importFrom dplyr group_by_at mutate
+#' @importFrom purrr map
+#' @importFrom tidyr nest unnest '%>%'
 #'
 #' @examples
 #' move_data1 <- data.frame(x = rnorm(10), y = rnorm(10), Individual = "Subject1", step = 1:10)
 #' move_data2 <- data.frame(x = rnorm(10), y = rnorm(10), Individual = "Subject2", step = 1:10)
-#' get_step_metrics_multiple(rbind(move_data1, move_data2), ID = "Individual", x = "x", y = "y", step_number = "step"
+#' get_step_metrics_multiple(rbind(move_data1, move_data2), id = "Individual", x = "x", y = "y", step_number = "step")
 get_step_metrics_multiple <- function(movement_data, id = "ID", x = "X", y = "Y", step_number = NA) {
   movement_data <- movement_data %>%
     group_by_at(id) %>%
